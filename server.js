737ab9cee52f8f43923412dc3bf5d4e188728ace
melkie yilk <melkieyilk@gmail.com>
2025-03-06 01:09:37 +0300

    modify the overall

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const pharmacyRoutes = require("./routes/pharmacyRoutes");
+const userRoutes = require("./routes/userRoutes");
+const drugStoreRoutes = require("./routes/drugStoreRoutes");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const cors = require("cors"); // Import cors
 
+const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Set up routes
+app.use("/pharmacies", pharmacyRoutes); // Pharmacy-related routes
+app.use("/users", userRoutes); // User-related routes
+app.use("/drugstore", drugStoreRoutes); // Drugstore-related routes
+
+// Server setup
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
